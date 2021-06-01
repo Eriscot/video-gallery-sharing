bc2b51be152c3e988ca310437540b5f15666b827
eriscot <alexsun123777@gmail.com>
2021-06-01 13:38:13 +0300

    feat: swagger set up

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Video Gallery Sharing')
+    .setDescription('The API for Video Gallery Sharing app!')
+    .setVersion('0.0.1')
+    .addTag('Video Sharing')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(3000);
 }
+
 bootstrap();
